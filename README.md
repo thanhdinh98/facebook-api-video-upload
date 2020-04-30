c68e170188e1be5c1074573ee3d3b6313f8ccad1
thanhdinh98 <dnthanh1298@gmail.com>
2020-04-30 11:40:51 +0700

    (update): update facebook-api-video-upload

--- README.md
+++ README.md
@@ -3,39 +3,52 @@
 
 ## Install
 ```
-$ npm i facebook-api-video-upload --save
+$ npm i fb-upload-video-api --save
 ```
 Tested on OS X and Linux.
 
 ## Usage
 ```javascript
 const fs = require('fs');
-const fbUpload = require('facebook-api-video-upload');
+const MovieUpload = require('facebook-api-video-upload');
+
+const emitter = new MovieUpload();
 
 const args = {
-  token: "YOURTOKEN", // with the permission to upload
-  id: "YOURID", //The id represent {page_id || user_id || event_id || group_id}
-  stream: fs.createReadStream('./fixture.mp4'), //path to the video,
-  title: "my video",
-  description: "my description",
-  thumb: {
-    value: fs.createReadStream('./fixture.jpg'),
-    options: {
-      filename: 'fixture.jpg',
-      contentType: 'image/jpg'
-    }
-  }
-  // if you want the default thumb from the video just remove the field
-  // you can add any extra fields from the api https://developers.facebook.com/docs/graph-api/reference/page/videos/#Creating
-  // all keys except token, id, stream are passed to the final request
+  token: "YOURTOKEN",  // with the permission to upload
+  id: "YOURID",       //The id represent {page_id || user_id || event_id || group_id}
+  videoSize: size    // integer
 };
 
-fbUpload(args).then((res) => {
-  console.log('res: ', res);
-  //res:  { success: true, video_id: '1838312909759132' }
-}).catch((e) => {
-  console.error(e);
-});```
+// Recieve start_offset and end_offset of facebook init and upload_chunk api
+emitter.on('slice_chunk', (start, end)=>{
+  const chunkStream = fs.createReadStream('./fixture.mp4', {
+    start: start, //integer
+    end: end     // integer
+  });
+  // Emit chunk to upload
+  emitter.emit('chunk', chunkStream)
+})
+
+// Show percent of uploading
+emitter.on('upload', (percent)=>{
+  console.log(percent);
+})
+
+emitter.on('done', (success, video_id)=>{
+  console.log(video_id);
+})
+
+emitter.on('error', (err)=>{
+  console.log(err);
+})
+
+emitter.uploadToFB(args);
+```
+
+## Features
+- Support upload videos larger than 2gb (maximun size of buffer in nodejs)
+- Don't need to load all video into buffer
 
 ## License
 MIT © [MrdotB](https://github.com/MRdotB)
--- index.js
+++ index.js
@@ -1,93 +1,128 @@
-const streamToPromise = require('stream-to-promise')
 const rp = require('request-promise')
+const { EventEmitter } = require('events')
 
 const url = 'https://graph-video.facebook.com'
 const version = 'v3.2'
 
-const retryMax = 10
-let retry = 0
+class UploadMovie extends EventEmitter {
+  constructor() {
+    super();
+    this.retry = 0
+    this.retryMax = 10
+  }
 
-function apiInit(args, videoSize) {
-  const options = {
-    method: 'POST',
-    uri: `${url}/${version}/${args.id}/videos?access_token=${args.token}`,
-    json: true,
-    form: {
-      upload_phase: 'start',
-      file_size: videoSize
+  async uploadToFB(args) {
+    try {
+      const {
+        video_id,
+        start_offset,
+        end_offset,
+        upload_session_id } = await this._appInit({ id: args.id, token: args.token }, args.videoSize);
+
+      this.start_offset = start_offset;
+      this.end_offset = end_offset;
+
+      this.on('chunk', (stream) => {
+        const bufs = [];
+        stream.on('data', (data) => {
+          bufs.push(data);
+        })
+        stream.on('end', () => {
+          this.chunk = Buffer.concat(bufs);
+          this.emit('upload_chunk', this.chunk);
+        })
+      })
+
+      this.on('upload_chunk', async (chunk) => {
+        try {
+          if (this.start_offset === this.end_offset) {
+            const { success } = await this._apiFinish({
+              id: args.id,
+              token: args.token
+            }, upload_session_id)
+            this.emit('done', success, video_id);
+          } else {
+            const res = await this._uploadChunk({
+              id: args.id,
+              token: args.token
+            }, upload_session_id, this.start_offset, chunk)
+            this.start_offset = res.start_offset;
+            this.end_offset = res.end_offset;
+            this.emit('upload', (this.end_offset * 100) / args.videoSize)
+            this.emit('slice_chunk', this.start_offset, this.end_offset);
+          }
+        } catch (err) {
+          this.emit('error', err);
+        }
+      });
+
+      this.emit('slice_chunk', this.start_offset, this.end_offset);
+    } catch (err) {
+      this.emit('error', err);
     }
   }
 
-  return rp(options).then(res => res)
-}
-
-function apiFinish(args, upload_session_id, video_id) {
-  const {token, id, stream, ...extraParams} = args
-
-  const options = {
-    method: 'POST',
-    json: true,
-    uri: `${url}/${version}/${args.id}/videos`,
-    formData: {
-      ...extraParams,
-      upload_session_id,
-      access_token: args.token,
-      upload_phase: 'finish',
-    }
-  }
-
-  return rp(options)
-    .then(res => ({...res, video_id}))
-}
-
-function uploadChunk(args, id, start, chunk) {
-  const formData = {
-    access_token: args.token,
-    upload_phase: 'transfer',
-    start_offset: start,
-    upload_session_id: id,
-    video_file_chunk: {
-      value: chunk,
-      options: {
-        filename: 'chunk'
+  _appInit({ id, token }, videoSize) {
+    const options = {
+      method: 'POST',
+      uri: `${url}/${version}/${id}/videos?access_token=${token}`,
+      json: true,
+      form: {
+        upload_phase: 'start',
+        file_size: videoSize
       }
     }
-  }
-  const options = {
-    method: 'POST',
-    uri: `${url}/${version}/${args.id}/videos`,
-    formData: formData,
-    json: true
+
+    return rp(options);
   }
 
-  return rp(options)
-    .then(res => {
-      retry = 0
-      return res
-    })
-    .catch(err => {
-      if (retry++ >= retryMax) {
-        return err
+  _apiFinish({ id, token }, upload_session_id) {
+    const options = {
+      method: 'POST',
+      json: true,
+      uri: `${url}/${version}/${id}/videos`,
+      formData: {
+        upload_session_id,
+        access_token: token,
+        upload_phase: 'finish',
       }
-      return uploadChunk(args, id, start, chunk)
-    })
-}
+    }
 
-function uploadChain(buffer, args, res, ids) {
-  if (res.start_offset === res.end_offset) {
-    return ids
+    return rp(options)
   }
 
-  var chunk = buffer.slice(res.start_offset, res.end_offset)
-  return uploadChunk(args, ids[0], res.start_offset, chunk)
-    .then(res => uploadChain(buffer, args, res, ids))
+  _uploadChunk({ id, token }, upload_session_id, start_offset, chunk) {
+    const formData = {
+      access_token: token,
+      upload_phase: 'transfer',
+      start_offset: start_offset,
+      upload_session_id: upload_session_id,
+      video_file_chunk: {
+        value: chunk,
+        options: {
+          filename: 'chunk'
+        }
+      }
+    }
+    const options = {
+      method: 'POST',
+      uri: `${url}/${version}/${id}/videos`,
+      formData: formData,
+      json: true
+    }
+
+    return rp(options)
+      .then(res => {
+        this.retry = 0
+        return res
+      })
+      .catch(err => {
+        if (this.retry++ >= this.retryMax) {
+          throw err
+        }
+        return this._uploadChunk({ id, token }, upload_session_id, start_offset, chunk)
+      })
+  }
 }
 
-function facebookApiVideoUpload(args) {
-  return streamToPromise(args.stream)
-    .then(buffer => Promise.all([buffer, apiInit(args, buffer.length)]))
-    .then(([buffer, res]) => uploadChain(buffer, args, res, [res.upload_session_id, res.video_id]))
-    .then(([id, video_id]) => apiFinish(args, id, video_id))
-}
-
-module.exports = facebookApiVideoUpload
+module.exports = UploadMovie;
